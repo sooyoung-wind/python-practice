a6a38e753608bead0f8f94f17e1c7f34a4b3b1c1
sooyoung-wind <sooyoung.wind@gmail.com>
2023-07-30 22:18:42 +0900

    Update

--- selenium_insta/Code04_refactoring_insta.py
+++ selenium_insta/Code04_refactoring_insta.py
@@ -20,15 +20,21 @@ driver = webdriver.Chrome(service = service, options=options)
 url = 'https://www.instagram.com'
 driver.get(url)
 
+
+def login(my_id, my_pw):
+    input_id = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
+    input_id.send_keys(my_id)
+    input_pw = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(my_pw)
+    input_wd = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button').click()
+
 time.sleep(3)
-input_id = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
-input_id.send_keys(my_id)
-input_pw = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(my_pw)
-input_wd = driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button').click()
+login(my_id, my_pw)
+
 
 # serach
 time.sleep(3)
-url = "https://www.instagram.com/explore/tags/나비/"
+hashtag = "강아지"
+url = f"https://www.instagram.com/explore/tags/{hashtag}/"
 driver.get(url)
 
 # scroll
@@ -36,7 +42,6 @@ time.sleep(3)
 for _ in range(5):
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     time.sleep(2)
-    
-    
+
 
 
