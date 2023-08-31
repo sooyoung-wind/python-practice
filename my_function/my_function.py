7c6504db3cda6a1f379f849c7588e9d58b3a8d7b
sooyoung-wind <sooyoung.wind@gmail.com>
2023-09-01 05:58:25 +0900

    Update my_function.py

--- my_function/my_function.py
+++ my_function/my_function.py
@@ -6,6 +6,8 @@ This code is a collection of functions that I need for performing EDA.
 
 # null_check function is no need to use it anymore.
 # you can use this code "data.loc[data.isnull().any(axis=1)] "
+
+
 def null_check(data):
     """
     This function checks for null data in dataframe of pandas.
@@ -49,3 +51,55 @@ def unique_check(data, print_len=5):
             unique_list.append(temp_column[0:len(data.columns):step])
     output = {x: y for x, y in zip(data.columns, unique_list)}
     return output
+
+
+# Outlier detection was performed
+# define my method, quantile
+def my_outlier_IQR(data):
+    """
+    Parameters
+    ----------
+    data : pandas Series or List
+        this is outlier using IOR
+         Q3 + 1.5 * IQR >= x >= Q1 - 1.5 * IQR. is False         
+
+    Returns
+    -------
+    outlier_list : List type 
+        True or False List.
+    """
+    Q1 = data.quantile(0.25)
+    Q3 = data.quantile(0.75)
+    IQR = Q3 - Q1
+
+    outlier_list = []
+    for x_data in data:
+        if x_data > Q3 + 1.5 * IQR or x_data < Q1 - 1.5 * IQR:
+            outlier_list.append(True)
+        else:
+            outlier_list.append(False)
+    return outlier_list
+
+
+def my_outlier_Zscore(data):
+    """
+    Parameters
+    ----------
+    data : pandas Series or List
+        this is outlier using Z-score
+         Q3 + 1.5 * IQR >= x >= Q1 - 1.5 * IQR. is False         
+
+    Returns
+    -------
+    outlier_list : List type 
+        True or False List.
+    """
+    threshold = 3
+
+    data_mean = data.mean()
+    data_std = data.std()
+
+    z_score = [(x_data - data_mean) / data_std for x_data in data]
+    mask = [abs(z) > threshold for z in z_score]  # True & False
+
+    return mask
