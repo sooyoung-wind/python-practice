5b632b4d6e5f942ad0d8a1969ed8402ed122b779
sooyoung-wind <sooyoung.wind@gmail.com>
2023-09-14 01:06:49 +0900

    연습코드 업데이트

--- EDA_project/practice/my_practice.py
+++ EDA_project/practice/my_practice.py
@@ -1,5 +1,11 @@
-data = [1, 2, 3, 4]
-data2 = [[1,2],
-        [3,4]]
+def find_dog(sound):
+    if sound == "멍멍":
+        return("개가 짖네")
 
-arr = np.arrary(data)
+    else:
+        return("다른 동물이구나")
+
+sound = "야옹"
+find_result = find_dog(sound)
+
+print(find_result)
